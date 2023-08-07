2183c791213b48a7f265dae1446b1bd261d67cbe
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-07 05:12:32 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import { Button } from '@mui/material'
+
+
+const App = () => {
+  return (
+    <Button variant="contained" color="primary">Hello world</Button>
+  )
+}
+
+export default App
