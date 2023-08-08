d9bb4232a8945cba67361cd4411fb2a4ddf576ea
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-08 17:59:35 +0300

    returned it back

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -3,7 +3,7 @@ import { Route, Routes, Navigate } from 'react-router-dom'
 import { privateRoutes, publicRoutes } from '../routes'
 import { CHAT_ROUTE, LOGIN_ROUTE } from '../utils/consts'
 const AppRouter = () => {
-  const user = true
+  const user = false
   return (
     user ? (
       <Routes>
