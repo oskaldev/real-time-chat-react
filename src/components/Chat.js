b77b95a8a327386e43eb28b1d7852c2fc3a578fa
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-08 18:11:39 +0300

    returned it back

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Chat = () => {
   return (
-    <div>Chat2</div>
+    <div>Chat</div>
   )
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login2</div>
+    <div>Login</div>
   )
 }
 
