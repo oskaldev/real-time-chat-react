aa04f096cc9a2886da157d367636ad41f7337ca5
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-10 03:25:09 +0300

    feat: Initialize Firebase and set up entry point
    Configured Firebase initialization using environment variables and set up the initial entry point for the application.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+
+firebase.initializeApp(
+  {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  }
+)
+const Context = createContext(null)
+const auth = firebase.auth()
+const firestore = firebase.firestore()
 
 const root = createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+  <Context.Provider value={{
+    firebase,
+    firestore,
+    auth
+  }}>
+    <App />
+  </Context.Provider>
+)
 const devMode = process.env.NODE_ENV === 'development'
 if (devMode && module && module.hot) {
   module.hot.accept()
