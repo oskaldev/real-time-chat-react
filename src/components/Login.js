ebd1ff229d0cb56b35495d873b8f7d2655837ce6
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-10 03:16:58 +0300

    feat: Create Login page
    Added the Login page component with a Google login button for user authentication.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,22 @@
+import { Box, Button, Container, Grid } from '@mui/material'
 import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <Container>
+      <Grid container style={{ height: window.innerHeight - 50 }}
+        alignItems={'center'}
+        justifyContent={'center'}>
+        <Grid style={{ width: 400, background: 'lightgray' }}
+          container
+          alignItems={'center'}
+          direction={'column'}>
+          <Box p={5}>
+            <Button style={{ color: 'black', borderColor: 'black', margin: '0px 3px' }} variant='outlined'>Войти с помощью Google</Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 
