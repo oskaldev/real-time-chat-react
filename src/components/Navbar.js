f5b17c34105248ab415a04bdab568a399802db0e
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-10 03:15:17 +0300

    feat: Add Navbar component
    Added the Navbar component to provide navigation links and buttons based on user authentication status.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { AppBar, Button, Grid, Toolbar } from '@mui/material'
+import { NavLink } from 'react-router-dom'
+import { LOGIN_ROUTE } from '../utils/consts'
+const user = false
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <AppBar color={'secondary'} position="static">
+      <Toolbar variant='dense'>
+        <Grid container justifyContent={'flex-end'}>
+          {user ?
+            <Button style={{ color: 'white', borderColor: 'white', margin: '0px 3px' }} variant='outlined'>Выйти</Button>
+            :
+            <NavLink to={LOGIN_ROUTE}>
+              <Button style={{ color: 'white', borderColor: 'white', margin: '0px 3px' }} variant='outlined'>Логин</Button>
+            </NavLink>
+          }
+        </Grid>
+      </Toolbar>
+    </AppBar>
   )
 }
 
