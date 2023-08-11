8675f1c5730addd139f639ecf2a8f083b9c7da21
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-11 21:39:26 +0300

    Refactor user authentication handling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { AppBar, Button, Grid, Toolbar } from '@mui/material'
 import { NavLink } from 'react-router-dom'
 import { LOGIN_ROUTE } from '../utils/consts'
-const user = false
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { AuthContext } from '../index'
+
 
 
 const Navbar = () => {
+  const { auth } = useContext(AuthContext) || {}
+  const [ user ] = auth ? useAuthState(auth) : [ null ]
   return (
     <AppBar color={'secondary'} position="static">
       <Toolbar variant='dense'>
         <Grid container justifyContent={'flex-end'}>
           {user ?
-            <Button style={{ color: 'white', borderColor: 'white', margin: '0px 3px' }} variant='outlined'>Выйти</Button>
+            <Button onClick={() => auth.signOut()} style={{ color: 'white', borderColor: 'white', margin: '0px 3px' }} variant='outlined'>Выйти</Button>
             :
             <NavLink to={LOGIN_ROUTE}>
               <Button style={{ color: 'white', borderColor: 'white', margin: '0px 3px' }} variant='outlined'>Логин</Button>
