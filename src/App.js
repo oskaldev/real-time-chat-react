2f329649f95cc098c657888ce4d4363fffee4e5c
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-11 21:35:29 +0300

    Added loading handling using AuthContext

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import AppRouter from './components/AppRouter'
+import Loader from './components/Loader'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { AuthContext } from '.'
 
 
 const App = () => {
+  const { auth } = useContext(AuthContext)
+  const [ user, loading, error ] = useAuthState(auth)
+  if (loading) {
+    return <Loader />
+  }
   return (
     <BrowserRouter>
       <Navbar />
