ed4d44e384d9475a8b204bfa94eb3022efc90cf7
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-15 03:15:11 +0300

    configuring the structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const DotenvWebpackPlugin = require('dotenv-webpack')
 
 module.exports = {
   entry: './src/index.js',
@@ -32,6 +33,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './index.html',
     }),
+    new DotenvWebpackPlugin(),
   ],
   devServer: {
     historyApiFallback: true
