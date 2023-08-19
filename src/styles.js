0a5a015e49dac3d746b8aa3103e2947696f2283c
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-19 04:21:35 +0300

    add global styles with button MUI

---
+++ src/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import Button from '@mui/material/Button'
+export const StyleMuiButton = styled(Button)`
+&&&{
+    color: white;
+    border-color: white;
+    margin: 0 3px;
+}`
