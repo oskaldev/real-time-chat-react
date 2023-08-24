964f3557c06a950429519d08fd10e4b7f4a8a46b
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-24 04:21:11 +0300

    feat: Implement loading state handling in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,17 @@ import './App.css';
 import Navbar from '@components/common/Navbar';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from '@routing/AppRouter';
+import useAuth from '@hooks/useAuth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import Loader from '@components/common/Loader';
 
 const App = () => {
+  const { auth } = useAuth();
+  const [user, loading, error] = useAuthState(auth);
+
+  if (loading) {
+    return <Loader />;
+  }
   return (
     <BrowserRouter>
       <Navbar />
