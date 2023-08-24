8807072075e725378bcfac7cb2aefb5f1fcfcd0f
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-24 04:23:38 +0300

    feat: update Login component add handleLoginWithGoogle

--- src/components/screens/login/Login.jsx
+++ src/components/screens/login/Login.jsx
@@ -1,7 +1,27 @@
+import { Box, Button, Container, Grid } from '@mui/material';
 import React from 'react';
+import loginStyles from './loginStyles';
+import useAuth from '@hooks/useAuth';
+import { loginWithGoogle } from '@helpers/authHelpers';
 
 const Login = () => {
-  return <div>Login</div>;
+  const { auth } = useAuth();
+  const handleLoginWithGoogle = async () => {
+    await loginWithGoogle(auth);
+  };
+  return (
+    <Container>
+      <Grid container style={loginStyles.mainGrid} alignItems='center' justifyContent='center'>
+        <Grid style={loginStyles.buttonGrid} container alignItems='center' direction='column'>
+          <Box p={5}>
+            <Button onClick={handleLoginWithGoogle} style={loginStyles.button} variant='outlined'>
+              Войти с помощью Google
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Login;
