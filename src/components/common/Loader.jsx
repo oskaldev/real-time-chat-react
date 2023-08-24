e8ab291a031dcb7bf2b3ec144a8ab68b6b084de9
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-24 04:20:00 +0300

    feat: update Loader component

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Container, Grid } from '@mui/material';
+import React from 'react';
 
 const Loader = () => {
   return (
-    <div>Loader</div>
-  )
-}
+    <Container>
+      <Grid
+        container
+        style={{ height: window.innerHeight - 50 }}
+        alignItems={'center'}
+        justifyContent={'center'}>
+        <Grid container alignItems={'center'} direction={'column'}>
+          <div className='lds-dual-ring'></div>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
-export default Loader
+export default Loader;
