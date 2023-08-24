8c7f0838bc4d7d72ebf3acc528dd4e06aaa9c1f4
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-24 04:26:13 +0300

    Refactor code

--- src/HOC/AuthRouteHandler.jsx
+++ src/HOC/AuthRouteHandler.jsx
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '@providers/AuthProvider';
+import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { CHAT_ROUTE, LOGIN_ROUTE } from '@constans/consts';
+import useAuth from '@hooks/useAuth';
 
 const AuthRouteHandler = ({ privateRoutes, publicRoutes }) => {
-  const { auth } = useContext(AuthContext);
+  const { auth } = useAuth();
   const [user] = useAuthState(auth);
   const routesToRender = user ? privateRoutes : publicRoutes;
   const fallbackRoute = user ? CHAT_ROUTE : LOGIN_ROUTE;
