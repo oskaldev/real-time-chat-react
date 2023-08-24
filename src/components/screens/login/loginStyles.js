ef307849438cc6a2b10bf2ccac277e57e518e9b8
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-24 04:24:41 +0300

    feat: add styles with login component

---
+++ src/components/screens/login/loginStyles.js
@@ -0,0 +1,17 @@
+const loginStyles = {
+  mainGrid: {
+    height: 'calc(100vh - 50px)'
+  },
+  buttonGrid: {
+    width: 400,
+    background: '#9c27b0'
+  },
+  button: {
+    color: 'white',
+    borderColor: 'white',
+    margin: '0px 3px',
+  },
+
+}
+
+export default loginStyles
