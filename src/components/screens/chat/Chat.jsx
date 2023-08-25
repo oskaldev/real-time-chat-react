1cda1fc192b88314228edbc0e070196643378e21
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-25 18:50:25 +0300

    code review: add hook:useChat, Component:ChatMessage,ChatInput

--- src/components/screens/chat/Chat.jsx
+++ src/components/screens/chat/Chat.jsx
@@ -1,67 +1,32 @@
-import React, { useContext, useState } from 'react'
-import { AuthContext } from '..'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { Avatar, Button, Container, Grid, TextField } from '@mui/material'
-import { useCollectionData } from 'react-firebase-hooks/firestore'
-import Loader from './Loader'
-import firebase from 'firebase/compat/app'
-
+import React from 'react';
+import Loader from '@components/common/Loader';
+import { Container, Grid } from '@mui/material';
+import ChatInput from './chatInput';
+import useChat from '@hooks/useChat';
+import ChatMessage from './chatMessage';
 
 const Chat = () => {
-  const { auth, firestore } = useContext(AuthContext)
-  const [ user ] = useAuthState(auth)
-  const [ value, setValue ] = useState('')
-  const [ message, loading ] = useCollectionData(
-    firestore.collection('messages').orderBy('createdAt')
-  )
+  const { user, message, loading, sendMessage } = useChat();
 
-  const sendMessage = async () => {
-    firestore.collection('messages').add({
-      uid: user.uid,
-      displayName: user.displayName,
-      photoURL: user.photoURL,
-      text: value,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp()
-    })
-    setValue('')
-
-  }
   if (loading) {
-    return <Loader />
+    return <Loader />;
   }
   return (
     <Container>
-      <Grid container style={{ height: window.innerHeight - 50, marginTop: 2 }}
+      <Grid
+        container
+        style={{ height: window.innerHeight - 50, marginTop: 50 }}
         justifyContent={'center'}
         alignItems={'center'}>
         <div style={{ width: '80%', height: '70vh', border: '1px solid gray', overflowY: 'auto' }}>
-          {message.map(message =>
-            <div style={{
-              padding: 5,
-              margin: 10,
-              border: user.uid === message.uid ? '2px solid #9c27b0' : '2px dashed red',
-              marginLeft: user.uid === message.uid ? 'auto' : '10px',
-              width: 'fit-content'
-            }}>
-              <Grid container >
-                <Avatar src={message.photoURL} />
-                <div>{message.displayName}</div>
-              </Grid>
-              <div>{message.text}</div>
-            </div>
-          )}
+          {message.map((message, index) => (
+            <ChatMessage key={index} message={message} user={user} />
+          ))}
         </div>
-        <Grid container direction='column' alignItems='flex-end'
-          style={{ width: '80%' }}
-        >
-          <TextField variant='outlined' fullWidth maxRows={2} value={value}
-            onChange={e => setValue(e.target.value)} />
-          <Button onClick={sendMessage} variant='outlined' style={{ color: 'black', marginTop: 10 }}>Отправить</Button>
-
-        </Grid>
+        <ChatInput onSendMessage={sendMessage} />
       </Grid>
     </Container>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
